c943be27ad100b1ef620cef77f9a099c5d309237
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-10-25 10:15:55 -0700

    Initial Commit

---
+++ Polaris/PolarisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarisApp.swift
+//  Polaris
+//
+//  Created by Cyril Zakka on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PolarisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
